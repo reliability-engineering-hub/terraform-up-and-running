8a2676850e7261dea14b90815a92cb79c2c43a29
yongckim <zxcv9203@naver.com>
2023-05-05 21:07:37 +0900

    :tada : 챕터 6 : 프로덕션 수준의 테라폼 코드 정리

---
+++ yckim/chapter6/code/live/stage/services/hello-world-app/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 0.12, < 0.13"
+}
+
+provider "aws" {
+  region = "us-east-2"
+
+  # Allow any 2.x version of the AWS provider
+  version = "~> 2.0"
+}
+
+module "hello_world_app" {
+
+	// 예시로 사용자의 github 주소로 사용해야함
+  source = "git@github.com:foo/modules.git//services/hello-world-app?ref=v0.0.5"
+
+  server_text = var.server_text
+
+  environment            = var.environment
+  db_remote_state_bucket = var.db_remote_state_bucket
+  db_remote_state_key    = var.db_remote_state_key
+
+  instance_type      = "t2.micro"
+  min_size           = 2
+  max_size           = 2
+  enable_autoscaling = false
+}
